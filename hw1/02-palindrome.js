0a14d4f53c621fbade15f4e9ac68679e1a7d07ff
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-04-08 20:12:50 -0700

    Added Javascript to check if input is a palindrome. Added Bootstrap to html to match styling on homework

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,35 @@
 const elem = document.querySelector('input');
+const resultDiv = document.querySelector('#result');
+const palindromeText = document.createElement("p");
+palindromeText.textContent = "";
+resultDiv.appendChild(palindromeText);
 
 elem.addEventListener('input', handleInput);
+
+function handleInput() {
+    if(isNaN(elem.value)){
+        palindromeText.textContent = "Palindrome must be a number. Try again."
+        palindromeText.style.color = "red"
+        return;
+    }
+    let stringVersion = elem.value.toString();
+    let stringReversed = reverseString(stringVersion);
+    if(elem.value > 0 && stringReversed === stringVersion)
+    {
+        palindromeText.textContent = "Yes. This is a palindrome!"
+        palindromeText.style.color = "green"
+    }
+    else{
+        palindromeText.textContent = "No. Try again."
+        palindromeText.style.color = "red"
+    }
+}
+
+function reverseString(stringToReverse) {
+    let reversedString = "";
+    for(let i = stringToReverse.length - 1; i >= 0; --i){
+        reversedString += stringToReverse[i];
+    }
+    return reversedString;
+}
+
