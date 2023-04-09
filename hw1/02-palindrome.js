94cfe6709cf007f7823b4ac5ee2e20c4d5c25e1f
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-04-08 21:02:05 -0700

    used linter to match airbnb style guide

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,34 +1,32 @@
 const elem = document.querySelector('input');
 const resultDiv = document.querySelector('#result');
-const palindromeText = document.createElement("p");
-palindromeText.textContent = "";
+const palindromeText = document.createElement('p');
+palindromeText.textContent = '';
 resultDiv.appendChild(palindromeText);
 
-elem.addEventListener('input', handleInput);
+function reverseString(stringToReverse) {
+  let reversedString = '';
+  for (let i = stringToReverse.length - 1; i >= 0; i -= 1) {
+    reversedString += stringToReverse[i];
+  }
+  return reversedString;
+}
 
 function handleInput() {
-    if (isNaN(elem.value)) {
-        palindromeText.textContent = "Palindrome must be a number. Try again."
-        palindromeText.style.color = "red"
-        return;
-    }
-    let stringVersion = elem.value.toString();
-    let stringReversed = reverseString(stringVersion);
-    if (elem.value > 0 && stringReversed === stringVersion) {
-        palindromeText.textContent = "Yes. This is a palindrome!"
-        palindromeText.style.color = "green"
-    }
-    else {
-        palindromeText.textContent = "No. Try again."
-        palindromeText.style.color = "red"
-    }
+  if (Number.isNaN(elem.value)) {
+    palindromeText.textContent = 'Palindrome must be a number. Try again.';
+    palindromeText.style.color = 'red';
+    return;
+  }
+  const stringVersion = elem.value.toString();
+  const stringReversed = reverseString(stringVersion);
+  if (elem.value > 0 && stringReversed === stringVersion) {
+    palindromeText.textContent = 'Yes. This is a palindrome!';
+    palindromeText.style.color = 'green';
+  } else {
+    palindromeText.textContent = 'No. Try again.';
+    palindromeText.style.color = 'red';
+  }
 }
 
-function reverseString(stringToReverse) {
-    let reversedString = "";
-    for (let i = stringToReverse.length - 1; i >= 0; --i) {
-        reversedString += stringToReverse[i];
-    }
-    return reversedString;
-}
-
+elem.addEventListener('input', handleInput);
