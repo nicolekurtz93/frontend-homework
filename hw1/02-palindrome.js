6d2a15a53846659c0a445edb3e2d070059de94e3
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-04-08 20:31:09 -0700

    cleaned up with Prettier

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -7,19 +7,18 @@ resultDiv.appendChild(palindromeText);
 elem.addEventListener('input', handleInput);
 
 function handleInput() {
-    if(isNaN(elem.value)){
+    if (isNaN(elem.value)) {
         palindromeText.textContent = "Palindrome must be a number. Try again."
         palindromeText.style.color = "red"
         return;
     }
     let stringVersion = elem.value.toString();
     let stringReversed = reverseString(stringVersion);
-    if(elem.value > 0 && stringReversed === stringVersion)
-    {
+    if (elem.value > 0 && stringReversed === stringVersion) {
         palindromeText.textContent = "Yes. This is a palindrome!"
         palindromeText.style.color = "green"
     }
-    else{
+    else {
         palindromeText.textContent = "No. Try again."
         palindromeText.style.color = "red"
     }
@@ -27,7 +26,7 @@ function handleInput() {
 
 function reverseString(stringToReverse) {
     let reversedString = "";
-    for(let i = stringToReverse.length - 1; i >= 0; --i){
+    for (let i = stringToReverse.length - 1; i >= 0; --i) {
         reversedString += stringToReverse[i];
     }
     return reversedString;
