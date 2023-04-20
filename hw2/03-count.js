2bea4794c584d3f3be3447193e0921faa5e63913
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-04-20 16:26:07 -0700

    Completed count and background portion

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,19 @@
 // Add your code here
+const input = document.querySelector('input');
+const content = document.querySelector('#content');
+const main = document.querySelector('main');
+const result = document.createElement('div');
+main.appendChild(result);
 
-input.addEventListener('keydown', handleKeyDown);
+function handleKeyDown() {
+  const userInput = input.value;
+  const InitalTextHtml = content.innerHTML;
+  content.style.display = 'None'; // Doing this so I don't lose my original text
+  result.innerHTML = InitalTextHtml; // Resetting the inner HTML to prevent duplicate marks
+  if (userInput !== '') {
+    const HighlightedText = InitalTextHtml.replaceAll(userInput, `<mark>${userInput}</mark>`);
+    result.innerHTML = HighlightedText;
+  }
+}
+
+input.addEventListener('input', handleKeyDown);
--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,28 @@
 // Add your code here
+const Button = document.querySelector('button');
+const Input = document.querySelector('input');
+const Body = document.querySelector('body');
+let IntervalId;
+
+function setBackgroundColor() {
+  const val1 = Math.random() * 255;
+  const val2 = Math.random() * 255;
+  const val3 = Math.random() * 255;
+  Body.style.backgroundColor = `rgba(${val1}, ${val2}, ${val3}, 0.3)`;
+}
+
+function handleButtonSubmission() {
+  if (Button.innerText === 'Start') {
+    Button.innerText = 'Stop';
+    Button.classList = 'btn btn-danger';
+
+    IntervalId = setInterval(setBackgroundColor, Input.value);
+  } else {
+    clearInterval(IntervalId);
+    IntervalId = null;
+    Button.innerText = 'Start';
+    Button.classList = 'btn btn-primary';
+  }
+}
+
+Button.addEventListener('click', handleButtonSubmission);
