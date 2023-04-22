8882667b9a51253051a95604c39a0eaf90f47668
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-04-21 21:14:57 -0700

    Wrapped up form exercise

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -13,16 +13,16 @@ function handleSubmission(evnt) {
   );
   console.log(`${email.name} : ${email.value ? email.value : 'no submission'}`);
   console.log(`${registration.name} : ${registration.value ? registration.value : 'no submission'}`);
-  if (fullStackInput.checked === true) {
-    courseList.push(fullStackInput.labels[0].outerText);
+  if (languagesInput.checked === true) {
+    courseList.push(languagesInput.labels[0].outerText);
   }
   if (operatingInput.checked === true) {
     courseList.push(operatingInput.labels[0].outerText);
   }
-  if (languagesInput.checked === true) {
-    courseList.push(languagesInput.labels[0].outerText);
+  if (fullStackInput.checked === true) {
+    courseList.push(fullStackInput.labels[0].outerText);
   }
-  console.log(`${courses[0].name} : ${courseList.length > 0 ? courseList : 'No Submission'}`);
+  console.log(`${courses[0].name} :\n ${courseList.length > 0 ? courseList : 'No Submission'}`);
   console.log(`${comment.name} : ${comment.value ? comment.value : 'no submission'}`);
   console.groupEnd(label);
 }
