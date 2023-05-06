72ca9272971524ba7d8b348ee33c4b54bb2ce24e
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-05-06 14:04:46 -0700

    exercise 2

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,45 +1,36 @@
 const backgroundColors = [
-  'rgba(54, 162, 235, 0.8)',
-  'rgba(255, 206, 86, 0.8)',
-  'rgba(255, 99, 132, 0.8)',
-  'rgba(75, 192, 192, 0.8)',
-  'rgba(153, 102, 255, 0.8)',
-  'rgba(255, 159, 64, 0.8)',
-  'rgba(199, 199, 199, 0.8)',
-  'rgba(83, 102, 255, 0.8)',
-  'rgba(40, 159, 64, 0.8)',
-  'rgba(210, 199, 199, 0.8)',
-  'rgba(78, 52, 199, 0.8)',
 ];
 
 const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
 ];
-
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+// const houseNames = new Map([
+//   ['Targaryen', 0], ['Tyrell', 0], ['Arryn', 0], ['GreyJoy', 0], ['Tully', 0], ['Stark', 0],
+//   ['Baratheon', 0], ['Frey', 0]]);
+const houseNames = new Map([]);
+
+function setChartColors(numberOfItems) {
+  for (let index = 0; index < numberOfItems; (index += 1)) {
+    const val1 = Math.random() * 255;
+    const val2 = Math.random() * 255;
+    const val3 = Math.random() * 255;
+    backgroundColors.push(`rgba(${val1}, ${val2}, ${val3}, 0.8)`);
+    borderColors.push(`rgba(${val1}, ${val2}, ${val3}, 1)`);
+  }
+}
 
 const renderChart = () => {
   const donutChart = document.querySelector('.donut-chart');
-
+  setChartColors(houseNames.size);
   const chart = new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: Array.from(houseNames.keys()),
       datasets: [
         {
           label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          data: Array.from(houseNames.values()),
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -50,16 +41,43 @@ const renderChart = () => {
 };
 
 const asyncGetThrone = async () => {
-  const promise = new Promise((resolve, reject) => {
-
-  });
   try {
-    const response = await promise;
-    console.log(response);
-
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
   } catch (error) {
-    console.error(error);
+    console.log('Error', error);
+    return null;
   }
 };
 
+const doesElementMatchAnotherHouse = (element) => {
+  houseNameKeys = Array.from(houseNames.keys());
+  houseNameKeys.forEach((house) => {
+
+  });
+};
+
+const parseThronesData = (thronesData) => {
+  thronesData.forEach((element) => {
+    if (houseNames.has(element.lastName)) {
+      houseNames.set(element.lastName, houseNames.get(element.lastName) + 1);
+    } else if (element.lastName !== '' && element.lastName !== 'None' && element.lastName !== 'Unknown') {
+      houseNames.set(element.lastName, 1);
+    }
+  });
+  const arrayOfMap = Array.from(houseNames);
+  console.log(arrayOfMap);
+  arrayOfMap.forEach((element) => {
+    if (element[1] == 1) {
+      doesElementMatchAnotherHouse(element);
+    }
+  });
+
+  renderChart();
+};
+
 renderChart();
+asyncGetThrone()
+  .then((thronesData) => parseThronesData(thronesData));
